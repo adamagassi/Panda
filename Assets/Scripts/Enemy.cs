db06227358fed886bed9b7fcf007e44e7cc7aa92
unknown-user <unknown-user@fe80::60d2:b387:368d:49e6%19>
2016-12-26 10:24:23 +0200

    Initial check-in of module PaintAPanda

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	[System.Serializable]
+	public class EnemyStats {
+		public int health = 10;
+	}
+	public EnemyStats enemyStats = new EnemyStats ();
+
+	public void DamageEnemy (int damage)
+	{
+		enemyStats.health -= damage;
+		if(enemyStats.health <= 0){
+			//GameManager.KillEnemy (this);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager gm ;
+	// Use this for initialization
+	void Start () {
+		if(gm==null){
+			//gm=new GameManager=
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
